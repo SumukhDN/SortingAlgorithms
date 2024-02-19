8172d4f2de6fce941ffa7e19766b14b162f92d57
SUMUKH D N <158539917+SumukhDN@users.noreply.github.com>
2024-02-19 19:56:39 +0530

    Update MergeSort.java

--- ComparisionBasedSorting/MergeSort/MergeSort.java
+++ ComparisionBasedSorting/MergeSort/MergeSort.java
@@ -7,8 +7,7 @@ class MergeSot {
 	// Merges two subarrays of arr[].
 	// First subarray is arr[l..m]
 	// Second subarray is arr[m+1..r]
-	void merge(int arr[], int l, int m, int r)
-	{
+	void merge(int arr[], int l, int m, int r){
 		// Find sizes of two subarrays to be merged
 		int n1 = m - l + 1;
 		int n2 = r - m;
@@ -59,8 +58,7 @@ class MergeSot {
 
 	// Main function that sorts arr[l..r] using
 	// merge()
-	void sort(int arr[], int l, int r)
-	{
+	void sort(int arr[], int l, int r){
 		if (l < r) {
 
 			// Find the middle point
@@ -76,8 +74,7 @@ class MergeSot {
 	}
 
 	// A utility function to print array of size n
-	static void printArray(int arr[])
-	{
+	static void printArray(int arr[]){
 		int n = arr.length;
 		for (int i = 0; i < n; ++i)
 			System.out.print(arr[i] + " ");
@@ -85,8 +82,7 @@ class MergeSot {
 	}
 
 	// main function
-	public static void main(String args[])
-	{
+	public static void main(String args[]){
 		int arr[] = { 12, 11, 13, 5, 6, 7 };
 
 		System.out.println("Given array is");
